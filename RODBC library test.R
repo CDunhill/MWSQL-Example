b7e74d67fab6c9174c972518a19c1299261a80cc
Chris Dunhill <chrisdunhill@gmail.com>
2017-11-15 16:15:13 +0000

    Template script

--- RODBC library test.R
+++ RODBC library test.R
@@ -1,1 +1,5 @@
-# Test
+
+library(RODBC)
+dbhandle <- odbcDriverConnect('driver={SQL Server};server=MWSQL;database=DataWarehouse;trusted_connection=true')
+res <- sqlQuery(dbhandle, "select * from branches")
+res
